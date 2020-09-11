7a1ec1d2cc8440c5086bdff21b99d420054fbf12
Christian Marquez <christianmarquez41@gmail.com>
2020-09-10 22:35:15 -0400

    erased all if statements, going back to drawingboard

--- script.js
+++ script.js
@@ -5,17 +5,30 @@ var generateBtn = document.querySelector("#generate");
 var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numb = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var specialChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
+var specialChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
 // array of the character arrays
 var ultimateArray = [lowercase, uppercase, numb, specialChar];
 // array where I will put the password
 var combinedArray = [];
 
-var confirmSpecialChar;
-var confirmUppercase;
-var confirmLowercase;
-var confirmNumbers;
 
+var confirmSpecialChar = false;
+var confirmUppercase = false;
+var confirmLowercase = false;
+var confirmNumbers = false;
+
+function generateChar() {
+  // random number between 0 and length of ultimateArray - 1
+  var arrayIndex1 = Math.floor(Math.random() * ultimateArray.length);
+  alert("value of arrayIndex1" , arrayIndex1);
+  // random number between 0 and length of a random array length inside the ultimate array - 1
+  var arrayIndex2 = Math.floor(Math.random() * ultimateArray[arrayndex1].length);
+  alert("value of arrayIndex2");
+  // getting a random character out of all arrays
+  var newChar = ultimateArray[arrayIndex1][arrayIndex2];
+
+  return newChar;
+}
 
 
 
@@ -25,6 +38,7 @@ function generatePassword() {
   // password not large enough
   if (numberOfChars < 8) {
     alert("Password length must be at least 8 characters");
+    generateChar();
     return "Try Again!"
   }
   // password too large
@@ -34,71 +48,18 @@ function generatePassword() {
   }
   // if user enters correct value, run this code!
   else {
+
     // ask user if they wants special characters
     confirmSpecialChar = confirm("Click OK if you want special characters in your password");
-    // user WANTS special characters
-    if (confirmSpecialChar) {
-      // ask user if they want numbers
-      confirmNumbers = confirm("Click OK if you want numbers in your password");
-      // user WANTS numbers
-      if (confirmNumbers) {
-        // ask user if they want lowercase letters
-        confirmLowercase = confirm("Click OK if you want lowercase letters");
-        // user WANTS lowercase letters
-        if (confirmLowercase) {
-          // ask user if they want uppercase letters
-          confirmUppercase = confirm("Click OK if you want uppercase letters");
-          // user WANTS uppercase letters
-          if (confirmUppercase) {
+ 
+    // ask user if they want numbers
+    confirmNumbers = confirm("Click OK if you want numbers in your password");
 
-
-          }
-          // user DOESN'T want uppercase letters
-          else {
-
-
-          }
-
-        }
-        // user DOESN't want lowercase letters
-        else {
-
-
-        }
-
-    
-
-
-
-      }
-      // user DOESN'T want numbers
-      else {
-
-
-      }
-
-
-
-     
-
-    
-    }
-
-    // user DOESN'T want special characters
-    else {
-
-    }
-
-
-
-
-
-
-
-  }
-
-
-
+    // ask user if they want lowercase letters
+    confirmLowercase = confirm("Click OK if you want lowercase letters in your password");
+ 
+    // ask user if they want uppercase letters
+    confirmUppercase = confirm("CLick OK if you want uppercase letters in your password");
 
 
 
