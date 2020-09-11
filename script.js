6ac4c9587bfd7d70a9d0c82d616b477d0007a8d3
Christian Marquez <christianmarquez41@gmail.com>
2020-09-10 20:30:32 -0400

    added if statements to rule out invalid inputs from user

--- script.js
+++ script.js
@@ -1,12 +1,38 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// arrays where I will pull the password from
 var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numb = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
+// array where I will put the password
+var combinedArray = [];
 
 function generatePassword() {
+  // asking how big does the user want the password to be
+  var numberOfChars = prompt("How many characters would you like your password to contain?");
+
+  if (numberOfChars < 8) {
+    alert("Password length must be at least 8 characters");
+
+    return "Try Again!"
+  }
+  else if (numberOfChars > 128) {
+    alert("Password length must be less than 129 characters");
+    
+    return "Try Again!"
+  }
+  else {
+
+
+
+
+
+
+  }
+
+
 
 
 
